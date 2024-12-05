fe00f2d807706685ba2dc5d76c41b5f33a0cf21f
Eliza Zhang <eliza@elizazhang.com>
2024-12-05 00:32:19 -0800

    Day 1 part 2

--- cmd/Day1.hs
+++ cmd/Day1.hs
@@ -2,9 +2,14 @@ module Main (main) where
 
 import Relude
 
+import Data.Map.Strict qualified as Map
+
 import Advent.Parse (eof, hspace1, intP, newline, parsePuzzleInput, someTill)
 
 main :: IO ()
 main = do
   (xs, ys) <- parsePuzzleInput "data/1" $ unzip <$> someTill ((,) <$> intP <* hspace1 <*> intP <* newline) eof
-  print $ sum ((\(x, y) -> abs (x - y)) <$> zip (sort xs) (sort ys))
+  putStrLn $ "Part 1: " <> show (sum ((\(x, y) -> abs (x - y)) <$> zip (sort xs) (sort ys)))
+  let counts = foldl' (\m y -> Map.insertWith (+) y 1 m) mempty ys
+      similarities = (\x -> x * fromMaybe 0 (Map.lookup x counts)) <$> xs
+  putStrLn $ "Part 2: " <> show (sum similarities)
