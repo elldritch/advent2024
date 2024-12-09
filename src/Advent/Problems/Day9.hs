181d5ef5638837b223650be9e3499a3f0d09b1f7
Eliza Zhang <eliza@elizazhang.com>
2024-12-09 02:42:52 -0800

    Day 9 part 2

--- src/Advent/Problems/Day9.hs
+++ src/Advent/Problems/Day9.hs
@@ -1,36 +1,66 @@
 module Advent.Problems.Day9 (parse, solve) where
 
 import Relude
+import Relude.Extra (bimapBoth)
 
-import Data.Vector (Vector, (!))
-import Data.Vector qualified as V
+import Data.Sequence qualified as Seq
 
 import Advent.Parse (Parser, digitP, eof, manyTill, newline)
+import Data.Sequence (findIndexL, index)
 
-parse :: Parser (Vector Block)
+parse :: forall f. (Applicative f, Monoid (f Segment)) => Parser (f Segment)
 parse = go . zip [0 ..] <$> manyTill digitP (newline >> eof)
  where
-  go :: [(Int, Int)] -> Vector Block
+  go :: [(Int, Int)] -> f Segment
   go [] = mempty
-  go ((i, n) : xs) = V.replicate n (if even i then FileID (i `div` 2) else Empty) <> go xs
+  go ((i, n) : xs) = pure (Segment (if even i then FileID (i `div` 2) else Empty) n) <> go xs
 
 data Block = Empty | FileID Int
-  deriving (Show)
+  deriving stock (Show, Eq)
 
-solve :: Vector Block -> (Int, Int)
-solve blocks =
-  ( sum $ zipWith (*) [0 ..] $ toList compacted
-  , undefined
-  )
+data Segment = Segment {block :: Block, length :: Int}
+  deriving stock (Show, Eq)
+
+toBlocks :: (Foldable t, Applicative f, Monoid (f Block)) => t Segment -> f Block
+toBlocks = foldl' (\acc x -> acc <> if x.length > 0 then stimes x.length (pure x.block) else mempty) mempty
+
+solve :: Seq Segment -> (Int, Int)
+solve segments = bimapBoth checksum (blockCompacted, fileCompacted)
  where
-  compacted = V.unfoldr unfoldCompacted (0, length blocks - 1)
+  blocks = toBlocks segments
 
-  unfoldCompacted :: (Int, Int) -> Maybe (Int, (Int, Int))
-  unfoldCompacted (i, i') =
-    if i > i'
-      then Nothing
-      else case blocks ! i of
-        Empty -> case blocks ! i' of
-          Empty -> unfoldCompacted (i, i' - 1)
-          FileID n -> Just (n, (i + 1, i' - 1))
-        FileID n -> Just (n, (i + 1, i'))
+  blockCompacted = unfoldr compactBlocks (0, length blocks - 1)
+   where
+    compactBlocks (i, i') | i > i' = Nothing
+    compactBlocks (i, i') = case blocks `index` i of
+      Empty -> case blocks `index` i' of
+        Empty -> compactBlocks (i, i' - 1)
+        FileID n -> Just (FileID n, (i + 1, i' - 1))
+      FileID n -> Just (FileID n, (i + 1, i'))
+
+  fileCompacted = toBlocks $ foldl' compactFile segments $ Seq.reverse segments
+   where
+    withIndex xs = Seq.zip (Seq.fromList $ take (length xs) [0 ..]) xs
+
+    compactFile :: Seq Segment -> Segment -> Seq Segment
+    compactFile segments' segment = case segment of
+      Segment Empty _ -> segments'
+      Segment (FileID n) l -> case leftmostSpace of
+        Nothing -> segments'
+        Just (i', s) ->
+          if i' < i
+            then
+              segments'
+                & Seq.update i (Segment Empty l)
+                & Seq.update i' (Segment (FileID n) l)
+                & (let remaining = s.length - l in if remaining > 0 then Seq.insertAt (i' + 1) (Segment Empty remaining) else id)
+            else segments'
+       where
+        i = fromMaybe (error "impossible: segment is missing") $ findIndexL (== segment) segments'
+        leftmostSpace = find (\(_, s) -> case s of Segment Empty l' -> l' >= l; _ -> False) $ withIndex segments'
+
+checksum :: (Foldable t) => t Block -> Int
+checksum blocks = snd $ foldl' f (0, 0) blocks
+ where
+  f (i, acc) (FileID n) = (i + 1, acc + i * n)
+  f (i, acc) Empty = (i + 1, acc)
--- test/Main.hs
+++ test/Main.hs
@@ -26,7 +26,7 @@ main = hspec $ do
     day 6 (41, 6) Day6.parse Day6.solve
     day 7 (3749, 11387) Day7.parse Day7.solve
     day 8 (14, 34) Day8.parse Day8.solve
-    day 9 (1928, undefined) Day9.parse Day9.solve
+    day 9 (1928, 2858) Day9.parse Day9.solve
  where
   day ::
     (Show part1, Show part2, Eq part1, Eq part2) =>
