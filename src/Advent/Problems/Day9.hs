6d61ff6a1f83005e469aafe0e9981ae53f37cc97
Eliza Zhang <eliza@elizazhang.com>
2024-12-13 00:10:57 -0800

    perf: Day 9 part 2 speedup

--- src/Advent/Problems/Day9.hs
+++ src/Advent/Problems/Day9.hs
@@ -1,9 +1,10 @@
 module Advent.Problems.Day9 (parse, solve) where
 
 import Relude
-import Relude.Extra (bimapBoth)
+import Relude.Extra (bimapBoth, toPairs)
 
-import Data.Sequence (findIndexL, index)
+import Data.Map.Strict qualified as Map
+import Data.Sequence (index)
 import Data.Sequence qualified as Seq
 
 import Advent.Parse (Parser, digitP, eof, manyTill, newline)
@@ -15,8 +16,12 @@ parse = go . zip [0 ..] <$> manyTill digitP (newline >> eof)
   go [] = mempty
   go ((i, n) : xs) = pure (Segment (if even i then FileID (i `div` 2) else Empty) n) <> go xs
 
-data Block = Empty | FileID Int
+type FileID = Int
+
+data Block = Empty | FileID FileID
   deriving stock (Show, Eq)
+
+type Address = Int
 
 data Segment = Segment {block :: Block, length :: Int}
   deriving stock (Show, Eq)
@@ -38,39 +43,43 @@ solve segments = bimapBoth checksum (blockCompacted, fileCompacted)
         FileID n -> Just (FileID n, (i + 1, i' - 1))
       FileID n -> Just (FileID n, (i + 1, i'))
 
-  fileCompacted = toBlocks $ foldl' compactFile segments $ Seq.reverse segments
+  fileCompacted = toList $ fst $ foldl' compactFile (blocks, emptySpaces) $ Seq.reverse segments
    where
-    withIndex xs = Seq.zip (Seq.fromList $ take (length xs) [0 ..]) xs
+    emptySpaces :: Map Int [Address]
+    emptySpaces = fst $ foldl' f (mempty, 0) segments
+     where
+      f (m, i) (Segment block len) = case block of
+        FileID _ -> (m, i + len)
+        Empty -> (Map.insertWith (flip (<>)) len [i] m, i + len)
 
-    compactFile :: Seq Segment -> Segment -> Seq Segment
-    compactFile segments' segment = case segment of
-      Segment Empty _ -> segments'
-      Segment (FileID n) l -> case leftmostSpace of
-        Nothing -> segments'
-        Just (i', s) ->
-          if i' < i
-            then
-              segments'
-                & Seq.update i (Segment Empty l)
-                & Seq.update i' (Segment (FileID n) l)
-                & (let remaining = s.length - l in if remaining > 0 then Seq.insertAt (i' + 1) (Segment Empty remaining) else id)
-            else segments'
+    segmentToBlocks :: Map FileID Address
+    segmentToBlocks = fst $ foldl' f (mempty, 0) segments
+     where
+      f (m, i) (Segment block len) = case block of
+        FileID n -> (Map.insert n i m, i + len)
+        Empty -> (m, i + len)
+
+    compactFile :: (Seq Block, Map Int [Address]) -> Segment -> (Seq Block, Map Int [Address])
+    compactFile (bs, empties) (Segment block len) = case block of
+      Empty -> (bs, empties)
+      FileID n -> case closestEmpty of
+        Nothing -> (bs, empties)
+        Just (_, []) -> (bs, empties)
+        Just (l, addr : addrs) -> if addr > oldBlockAddr then (bs, empties) else (bs'', empties')
+         where
+          oldBlockAddr = fromMaybe (error "unknown segment") $ Map.lookup n segmentToBlocks
+          bs' = foldl' (\b i -> Seq.update (oldBlockAddr + i) Empty b) bs [0 .. len - 1]
+          bs'' = foldl' (\b i -> Seq.update (addr + i) (FileID n) b) bs' [0 .. len - 1]
+          empties' = Map.insert l addrs empties & (if l > len then Map.insertWith (\new old -> sort $ new <> old) (l - len) [addr + len] else id)
        where
-        -- TODO: To improve performance, avoid re-finding indexes for the
-        -- segment every time. Instead, there should be a way to store this and
-        -- update whenever a new empty segment is partially consumed. Or perhaps
-        -- we should be storing block addresses, or mapping which block
-        -- addresses are occupied by which segments?
-        i = fromMaybe (error "segment is missing") $ findIndexL (== segment) segments'
-        -- TODO: To improve performance, avoid recalculating open spaces for
-        -- every segment. Instead, calculate all open spaces the first pass,
-        -- likely producing a map of open space size to the list of indexes
-        -- where those spaces start. Since new open spaces will never be created
-        -- (because the rightmost file is always moved first, and therefore new
-        -- open spaces will always be to the right of the next file being
-        -- moved), this map will never need to be added to (some spaces will
-        -- just need to be removed as they are used).
-        leftmostSpace = find (\(_, s) -> case s of Segment Empty l' -> l' >= l; _ -> False) $ withIndex segments'
+        closestEmpty :: Maybe (Int, [Address])
+        closestEmpty = viaNonEmpty head $ sortBy sortSpace $ toPairs $ Map.filterWithKey (\k _ -> k >= len) empties
+         where
+          sortSpace (_, as) (_, bs') = case (viaNonEmpty head as, viaNonEmpty head bs') of
+            (Nothing, Nothing) -> EQ
+            (Nothing, Just _) -> GT
+            (Just _, Nothing) -> LT
+            (Just a', Just b') -> compare a' b'
 
 checksum :: (Foldable t) => t Block -> Int
 checksum blocks = snd $ foldl' f (0, 0) blocks
